502e9bf8b47f3536e5193bd6e3d25e86e72eb129
Ed Thome <ethome@thoughtworks.com>
2018-04-10 13:14:36 -0400

    Score multiple rolls, failing test for spares

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,9 +1,31 @@
 export default class BowlingGame {
-  roll(pins) {
+  constructor() {
+    this.frames = []
+  } 
 
+  roll(pins) {
+    // this.frames = []
+    // this.frames = [[3]]p
+    if (this.frames.length > 0 && this.frames[this.frames.length-1].length <= 1) {
+      this.frames[this.frames.length-1].push(pins)
+    } else {
+      this.frames.push([pins])
+    }
   }
 
   score() {
-    return 3
+    let score = 0
+    
+    for(let i = 0; i < this.frames.length; i++) {
+      let theFrame = this.frames[i]
+      
+      for (let j=0; j < theFrame.length; j++){
+        score = score + theFrame[j]
+      }
+     
+    } 
+    return score
   }
 }
+
+// [[1, 3], [5, 5], [3, 6]]
--- src/BowlingGame.test.js
+++ src/BowlingGame.test.js
@@ -8,4 +8,27 @@ describe('BowlingGame', () => {
 
     expect(game.score()).toEqual(3)
   })
+
+  test('should add 2 rolls together', () => {
+    const game = new BowlingGame()
+   
+    game.roll(3)
+    game.roll(2)
+
+    expect(game.score()).toEqual(5)
+  })
+
+  test('spare should include value of next roll', () => {
+    const game = new BowlingGame()
+
+    game.roll(5)
+    game.roll(5)
+    game.roll(3)
+
+    expect(game.score()).toEqual(16)
+
+
+  })
+
+
 })
